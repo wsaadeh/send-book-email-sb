f60c5385e4a36ff2c3040d37ae4e403d15da3b6e
Wilson Saadeh <wilson_saadeh@hotmail.com>
2026-01-21 15:52:11 -0300

    project created

---
+++ src/main/java/com/saadeh/send_book_email_sb/SendBookEmailSbApplication.java
@@ -0,0 +1,13 @@
+package com.saadeh.send_book_email_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendBookEmailSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendBookEmailSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadeh/send_book_email_sb/SendBookEmailSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadeh.send_book_email_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendBookEmailSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
