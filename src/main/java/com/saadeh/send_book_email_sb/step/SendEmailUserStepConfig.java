66225b4fae8c62e0f10e1dd54d37e7930cca5884
Wilson Saadeh <wilson_saadeh@hotmail.com>
2026-01-21 18:02:15 -0300

    Basic configuration job

---
+++ src/main/java/com/saadeh/send_book_email_sb/step/SendEmailUserStepConfig.java
@@ -0,0 +1,29 @@
+package com.saadeh.send_book_email_sb.step;
+
+import com.saadeh.send_book_email_sb.domain.UserBookLoan;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class SendEmailUserStepConfig {
+
+    @Autowired
+    @Qualifier("transactionManagerApp")
+    private PlatformTransactionManager transactionManager;
+
+    @Bean
+    public Step sendEmailUserStep(ItemReader<UserBookLoan> readUserWithLoansCloseToReturnBook,
+            JobRepository jobRepository){
+        return new StepBuilder("sendEmailUserStep", jobRepository)
+                .<UserBookLoan, UserBookLoan>chunk(1,transactionManager)
+                .reader(readUserWithLoansCloseToReturnBook)
+                .build();
+    }
+}
