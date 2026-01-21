95333ec122be9a44dff9109a93cfd961160fa12a
Wilson Saadeh <wilson_saadeh@hotmail.com>
2026-01-21 17:35:28 -0300

    Domain classes and configure database

---
+++ src/main/java/com/saadeh/send_book_email_sb/domain/UserBookLoan.java
@@ -0,0 +1,51 @@
+package com.saadeh.send_book_email_sb.domain;
+
+import java.util.Date;
+
+public class UserBookLoan {
+    private User user;
+    private Book book;
+    private Date loan_date;
+
+    public UserBookLoan() {
+    }
+
+    public UserBookLoan(User user, Book book, Date loan_date) {
+        this.user = user;
+        this.book = book;
+        this.loan_date = loan_date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Date getLoan_date() {
+        return loan_date;
+    }
+
+    public void setLoan_date(Date loan_date) {
+        this.loan_date = loan_date;
+    }
+
+    @Override
+    public String toString() {
+        return "UserBookLoan{" +
+                "user=" + user +
+                ", book=" + book +
+                ", loan_date=" + loan_date +
+                '}';
+    }
+}
